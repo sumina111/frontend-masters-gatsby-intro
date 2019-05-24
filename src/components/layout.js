57d9777b64a2413a57031495fc02b8f3417f6116
sumina <suminaxresta@gmail.com>
2019-05-24 14:55:08 +0545

    form fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Global, css } from '@emotion/core';
-import './all.sass';
+// import './all.sass';
 import Helmet from 'react-helmet';
 import Header from './header';
 import useSiteMetadata from '../hooks/use-sitemetadata';
